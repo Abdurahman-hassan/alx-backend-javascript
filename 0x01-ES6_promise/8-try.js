a25baba87d8ae3b5f97a2795dbf44f9f74639e3c
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-29 09:03:02 +0300

    a function named divideFunction that will accept two arguments: numerator (Number) and denominator (Number).
    When the denominator argument is equal to 0, the function should throw a new error with the message cannot divide by 0. Otherwise it should return the numerator divided by the denominator.

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,12 @@
+/**
+ * Divides two numbers, throwing an error if the denominator is zero.
+ * @param {number} numerator - The numerator of the division.
+ * @param {number} denominator - The denominator of the division.
+ * @returns {number} The result of the division.
+ */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
