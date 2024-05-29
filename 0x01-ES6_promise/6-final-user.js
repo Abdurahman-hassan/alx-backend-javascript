db5f0cb1356e10aaafa59895d12e2ac88d01c86a
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-29 08:42:05 +0300

    enhance the code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,9 +3,9 @@ import uploadPhoto from './5-photo-reject.js'; // eslint-disable-line
 
 function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map(({ status, value, reason }) => ({
-      status,
-      value: status === 'fulfilled' ? value : reason,
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
     })));
 }
 
