7e04ddc36d84b0c10fa9009ea44683d55be95e64
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-29 08:43:56 +0300

    use export default async function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
-import signUpUser from './4-user-promise.js'; // eslint-disable-line
-import uploadPhoto from './5-photo-reject.js'; // eslint-disable-line
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
-
-export default handleProfileSignup;
