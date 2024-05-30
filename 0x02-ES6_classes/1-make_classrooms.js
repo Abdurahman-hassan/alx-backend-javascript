93d4078c8ca4801aa7ace1dba310ea055b0d3c51
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-05-31 00:35:07 +0300

    change the value of obj size

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom.js'; // eslint-disable-line
 export default function initializeRooms() {
   const classRoom1 = new ClassRoom(19);
   const classRoom2 = new ClassRoom(20);
-  const classRoom3 = new ClassRoom(21);
+  const classRoom3 = new ClassRoom(34);
 
   return [classRoom1, classRoom2, classRoom3];
 }
