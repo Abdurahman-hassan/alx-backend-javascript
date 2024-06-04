c419aca841a7c1b357efdd8f990c51c3405ca972
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-05 00:17:43 +0300

    a function getListStudentIds that returns an array of ids from a list of object.
    This function is taking one argument which is an array of objects - and this array is the same format as getListStudents from the previous task.
    
    If the argument is not an array, the function is returning an empty array.

---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,5 @@
+import getListStudentIds from "./1-get_list_student_ids.js"; //eslint-disable-line
+import getListStudents from "./0-get_list_students.js"; //eslint-disable-line
+
+console.log(getListStudentIds('hello'));
+console.log(getListStudentIds(getListStudents()));
