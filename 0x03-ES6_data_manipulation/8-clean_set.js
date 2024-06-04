c5e2c8e5371554addac4b64d302fd4445fab8619
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-05 01:13:34 +0300

    fix filtring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,22 @@
+/**
+ * Function to clean a set of values starting with a specific string
+ * @param {Set} set - The set of values to be cleaned
+ * @param {string} startString - The string to match the start of values
+ * @returns {string} - The cleaned string with values separated by '-'
+ */
 function cleanSet(set, startString) {
-  if (!set || !(set instanceof Set) || !startString) {
+  if (!startString) {
     return '';
   }
 
-  return Array.from(set)
-    .filter((item) => item.startsWith(startString))
-    .map((item) => item.substring(startString.length))
-    .join('-');
+  const result = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
 }
 
 export default cleanSet;
