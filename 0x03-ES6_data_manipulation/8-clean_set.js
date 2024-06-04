8f1976fc178abdd37b961286a7b090aab92baf90
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-05 01:15:52 +0300

    fix filtring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,19 +4,19 @@
  * @param {string} startString - The string to match the start of values
  * @returns {string} - The cleaned string with values separated by '-'
  */
-function cleanSet(set, startString) {
-  if (!startString) {
+export default function cleanSet(set, startString) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
 
-  const result = [];
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
     }
   }
-
-  return result.join('-');
+  return parts.join('-');
 }
-
-export default cleanSet;
