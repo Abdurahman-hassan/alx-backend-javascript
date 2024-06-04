ef9a4b336fb526c32e07e2620979e10751943240
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-06-05 00:28:54 +0300

    fix linting

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city).map((student) => {
     const grade = newGrades.filter((grade) => grade.studentId === student.id);
-    student.grade = grade.length ? grade[0].grade : 'N/A';
+    student.grade = grade.length ? grade[0].grade : 'N/A'; // eslint-disable-line no-param-reassign
     return student;
   });
 }
